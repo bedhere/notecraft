7c8bd969ba4fff125b4a159d7518ed2183f02eb7
baizihan <1278266826@qq.com>
2026-07-28 16:34:08 +0800

    refactor(desktop-ui): compact application title bar

--- shared/src/commonMain/kotlin/com/notecraft/ui/theme/AppSpacing.kt
+++ shared/src/commonMain/kotlin/com/notecraft/ui/theme/AppSpacing.kt
@@ -21,11 +21,18 @@ object AppSpacing {
     val listItemHorizontal = 8.dp
     val listItemVertical = 5.dp
     val editorPadding = 16.dp
+    val editorCompactPadding = 12.dp
     val sidebarWidth = 320.dp
+    val sidebarMinWidth = 260.dp
+    val sidebarNarrowWindowWidth = 920.dp
+    val sidebarAutoCollapseWidth = 720.dp
     val settingsWidth = 300.dp
-    val titleBarHeight = 54.dp
+    val titleBarHeight = 46.dp
     val editorHeaderHeight = 54.dp
+    val editorActionCompactWidth = 560.dp
+    val editorActionNarrowWidth = 360.dp
     val statusBarHeight = 30.dp
+    val statusCompactWidth = 420.dp
     val toolbarHeight = 36.dp
     val toolbarIconSize = 28.dp
     val segmentedButtonWidth = 176.dp
@@ -36,3 +43,4 @@ object AppSpacing {
     val previewMaxWidth = 960.dp
     val dividerHeight = 1.dp
 }
+
